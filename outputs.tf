3bfd5c003c5560402e4d7329b5d931fb3e83f092
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:08 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "security_center_settings" {
-  description = "All security_center_setting resources"
-  value       = azurerm_security_center_setting.security_center_settings
-}
 output "security_center_settings_enabled" {
-  description = "List of enabled values across all security_center_settings"
-  value       = [for k, v in azurerm_security_center_setting.security_center_settings : v.enabled]
+  description = "Map of enabled values across all security_center_settings, keyed the same as var.security_center_settings"
+  value       = { for k, v in azurerm_security_center_setting.security_center_settings : k => v.enabled }
 }
 output "security_center_settings_setting_name" {
-  description = "List of setting_name values across all security_center_settings"
-  value       = [for k, v in azurerm_security_center_setting.security_center_settings : v.setting_name]
+  description = "Map of setting_name values across all security_center_settings, keyed the same as var.security_center_settings"
+  value       = { for k, v in azurerm_security_center_setting.security_center_settings : k => v.setting_name }
 }
 
